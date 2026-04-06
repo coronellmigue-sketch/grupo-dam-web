719a99520dd4f9af5ac8e7f0533bbf485170d59f
coronellmigue-sketch <coronellmigue@gmail.com>
2026-04-06 12:35:35 -0500

    Add files via upload

--- usuarios.js
+++ usuarios.js
@@ -2,7 +2,8 @@
 const listaSocios = [
   { email: "admin@dam.com", cedula: "123456" },
   { email: "miguel@dam.com", cedula: "109876" },
-  { email: "socio1@dam.com", cedula: "2026" }
+  { email: "socio1@dam.com", cedula: "2026" },
+  { email: "kayucade99@gmail.com", cedula: "1102391031" },
 ];
 
 // Función para validar si existe el socio
