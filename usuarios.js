937d12dc4de61d922a512390cf64aaf8899c3586
coronellmigue-sketch <coronellmigue@gmail.com>
2026-05-06 15:47:27 -0500

    Add files via upload

--- usuarios.js
+++ usuarios.js
@@ -746,6 +746,7 @@ const listaSocios = [
   { email: "reykyrios@gmail.com", cedula: "1090438946" },
   { email: "mariobad09@gmail.com", cedula: "91293183" },
   { email: "reykyrios@gmail.com", cedula: "1090438946" },
+  { email: "hectorrueda1113@gmail.com", cedula: "91179828" },
 
   
 
