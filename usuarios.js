c72d9838df987766500a9d7a0d2c5e20ebdf7dea
coronellmigue-sketch <coronellmigue@gmail.com>
2026-06-10 13:44:57 -0500

    Add files via upload

--- usuarios.js
+++ usuarios.js
@@ -888,6 +888,7 @@ const listaSocios = [
   { email: "angelamariamoncadavera@gmail.com", cedula: "88273284" },
   { email: "carlos.stefania2504@gmail.com", cedula: "1098644003" },
   { email: "jhonylugo20@hotmail.com", cedula: "1095829469" },
+  { email: "arciniegasmarcela985@gmail.com", cedula: "1098742827" },
   
   
 
