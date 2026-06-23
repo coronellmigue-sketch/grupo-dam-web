9202cf69eec90f286623be9ab9393a81ee1c5c16
coronellmigue-sketch <coronellmigue@gmail.com>
2026-06-23 09:40:29 -0500

    Add files via upload

--- usuarios.js
+++ usuarios.js
@@ -405,6 +405,7 @@ const listaSocios = [
   { email: "edwardquinteroj@gmail.com", cedula: "1098785958" },
   { email: "daniela_saenz05@hotmail.com", cedula: "1098713686" },
   { email: "y_iseth@hotmail.es", cedula: "DAM2026" },
+  { email: "luisafernandabustos@hotmail.com", cedula: "1098759877" },
   
 
 ];
