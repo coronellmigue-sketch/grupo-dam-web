5932c948128f05039e661cb982edad4130c48453
coronellmigue-sketch <coronellmigue@gmail.com>
2026-06-30 10:40:14 -0500

    Add files via upload

--- usuarios.js
+++ usuarios.js
@@ -429,8 +429,9 @@ const listaSocios = [
   { email: "yulieth1525@gmail.com", cedula: "1093780602" },
   { email: "monroya820@gmail.com", cedula: "5692437" },
   { email: "keylacarrillo950@gmail.com", cedula: "1091377546" },
-  { email: "tatis.109986", cedula: "13567619" },
+  { email: "tatis.109986@gmail.com", cedula: "13567619" },
   { email: "pabonlidat2016@gmail.com", cedula: "49669988" },
+  { email: "smanuelaac@gmail.com", cedula: "1098803063" },
   
   
 
