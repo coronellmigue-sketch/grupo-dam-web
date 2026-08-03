ea67f681fb872078f995ca44d73f67293f74af89
coronellmigue-sketch <coronellmigue@gmail.com>
2026-08-03 13:39:15 -0500

    Add files via upload

--- usuarios.js
+++ usuarios.js
@@ -567,6 +567,7 @@ const listaSocios = [
   { email: "puntoreal@hotmail.com", cedula: "91249196" },
   { email: "guerdis137@gmail.com", cedula: "56098390" },
   { email: "chacal28@gmail.com", cedula: "74754505" },
+  { email: "mundialdecarretillas@gmail.com", cedula: "37746852" },
   
   
 
