9783542757da61773a7093f14f147e80a673091e
coronellmigue-sketch <coronellmigue@gmail.com>
2026-09-02 13:57:48 -0500

    Add files via upload

--- usuarios.js
+++ usuarios.js
@@ -636,6 +636,7 @@ const listaSocios = [
   { email: "angaritapapi@gmail.com", cedula: "1005339350" },
   { email: "carlosalbertosuarezforero@gmail.com", cedula: "91521318" },
   { email: "jeimy18ardila@gmail.com", cedula: "1098756075" },
+  { email: "jubelan2026@gmail.com", cedula: "91354965" },
   
   
 
